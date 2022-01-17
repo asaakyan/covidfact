1f942871a65d2774f542190a834fc4ae5deb976b
Arkadiy Saakyan <52617641+asaakyan@users.noreply.github.com>
2022-01-17 15:59:15 -0500

    Update gen_contrast_claims.py

--- contrastive_gen/gen_contrast_claims.py
+++ contrastive_gen/gen_contrast_claims.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 from transformers import RobertaTokenizerFast, RobertaForMaskedLM
 import pandas as pd
 
-fname = '' #'/content/drive/MyDrive/misinformation-NLP/results/additional_1k.csv'
+fname = '' #'/additional_1k.csv'
 output_fname = ''
 claims = pd.read_csv(fname)
 
@@ -11,13 +11,12 @@ model = RobertaForMaskedLM.from_pretrained("amoux/roberta-cord19-1M7k")
 
 fillmask = pipeline("fill-mask", model=model, tokenizer=tokenizer)
 
-#dir = '/content/drive/MyDrive/misinformation-NLP/results/'
 data = []
 i=0
 for claim, keyword, source, flair in tqdm(zip(claims['claim'], 
-													claims['keyword'], 
-													claims['src_url'],
-													claims['flair'])):
+					      claims['keyword'], 
+					      claims['src_url'],
+					      claims['flair'])):
 	i+=1
 	if claim[-1] != ".":
 		# if there is no . at the end and we are replacing last word, the model will just predict a punctuation mark
@@ -27,7 +26,7 @@ for claim, keyword, source, flair in tqdm(zip(claims['claim'],
 	#print(masked_text)
 
 	try:
-		suggs = fillmask(masked_text, top_k=10) #CHANGED TO 10
+		suggs = fillmask(masked_text, top_k=10) 
 		suggs = [sent['sequence'].replace("<s>", "").replace("</s>", "") for sent in suggs]
 		suggs = set(suggs)
 	except:
@@ -38,5 +37,5 @@ for claim, keyword, source, flair in tqdm(zip(claims['claim'],
 		  data.append([claim, keyword, sug, source, flair])
 
 
-all_fakes = pd.DataFrame(data, columns=['claim', 'keyword', 'false_claim', 'source', 'flair'])
-all_fakes.to_csv(dir+output_fname, index=False)
+df = pd.DataFrame(data, columns=['claim', 'keyword', 'false_claim', 'source', 'flair'])
+df.to_csv(dir+output_fname, index=False)
